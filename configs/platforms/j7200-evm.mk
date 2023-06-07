c962f6e1592279a0383d5ff3b8979218dca1ec89
Aniket Limaye <a-limaye@ti.com>
2023-06-07 20:07:48 +0530

    configs: platforms: Keep no whitespaces when setting variables to allow easier parsing of config files
    Signed-off-by: Aniket Limaye <a-limaye@ti.com>

--- configs/platforms/j7200-evm.mk
+++ configs/platforms/j7200-evm.mk
@@ -1,6 +1,6 @@
 #platform
-SOC = j7200
-SOC_TYPE ?= gp
+SOC=j7200
+SOC_TYPE?=gp
 
 #defconfig
 DEFCONFIG=tisdk_j7200-evm_defconfig
@@ -14,9 +14,9 @@ UBOOT_MACHINE_R5=j7200_evm_r5_config
 ifneq ($(SOC_TYPE),gp)
 	UBOOT_MACHINE=j7200_hs_evm_a72_config
 	UBOOT_MACHINE_R5=j7200_hs_evm_r5_config
-	SYSFW_SOC ?= j7200_sr2
+	SYSFW_SOC?=j7200_sr2
 endif
 
-KERNEL_DEVICETREE ?= ti/k3-j7200-common-proc-board.dtb     ti/k3-j7200-mcspi-loopback.dtbo	ti/k3-j7200-quad-port-eth-exp.dtbo
+KERNEL_DEVICETREE?= ti/k3-j7200-common-proc-board.dtb     ti/k3-j7200-mcspi-loopback.dtbo	ti/k3-j7200-quad-port-eth-exp.dtbo
 
-MAKE_ALL_TARGETS = arm-benchmarks am-sysinfo oprofile-example cryptodev u-boot sysfw-image linux linux-dtbs
+MAKE_ALL_TARGETS?= arm-benchmarks am-sysinfo oprofile-example cryptodev u-boot sysfw-image linux linux-dtbs
---
+++ configs/platforms/j721e-evm.mk
@@ -0,0 +1,22 @@
+#platform
+SOC=j721e
+SOC_TYPE?=gp
+
+#defconfig
+DEFCONFIG=tisdk_j7-evm_defconfig
+
+#u-boot machine
+UBOOT_MACHINE=j721e_evm_a72_config
+
+# Add CROSS_COMPILE and UBOOT_MACHINE for the R5
+UBOOT_MACHINE_R5=j721e_evm_r5_config
+
+ifneq ($(SOC_TYPE),gp)
+	UBOOT_MACHINE=j721e_hs_evm_a72_config
+	UBOOT_MACHINE_R5=j721e_hs_evm_r5_config
+	SYSFW_SOC?=j721e_sr1_1
+endif
+
+KERNEL_DEVICETREE?= ti/k3-j721e-common-proc-board.dtb     ti/k3-j721e-proc-board-tps65917.dtb     ti/k3-j721e-common-proc-board-infotainment.dtbo     ti/k3-j721e-cpb-csi2-ov5640.dtbo     ti/k3-j721e-fpdlink-cpb-fusion.dtbo     ti/k3-j721e-fpdlink-imx390-cm-0-0.dtbo     ti/k3-j721e-fpdlink-imx390-cm-0-1.dtbo     ti/k3-j721e-fpdlink-imx390-cm-0-2.dtbo     ti/k3-j721e-fpdlink-imx390-cm-0-3.dtbo     ti/k3-j721e-fpdlink-imx390-cm-1-0.dtbo     ti/k3-j721e-fpdlink-imx390-cm-1-1.dtbo     ti/k3-j721e-fpdlink-imx390-cm-1-2.dtbo     ti/k3-j721e-fpdlink-imx390-cm-1-3.dtbo     ti/k3-j721e-fpdlink-imx390-rcm-0-0.dtbo     ti/k3-j721e-fpdlink-imx390-rcm-0-1.dtbo     ti/k3-j721e-fpdlink-imx390-rcm-0-2.dtbo     ti/k3-j721e-fpdlink-imx390-rcm-0-3.dtbo     ti/k3-j721e-fpdlink-imx390-rcm-1-0.dtbo     ti/k3-j721e-fpdlink-imx390-rcm-1-1.dtbo     ti/k3-j721e-fpdlink-imx390-rcm-1-2.dtbo     ti/k3-j721e-fpdlink-imx390-rcm-1-3.dtbo     ti/k3-j721e-sk.dtb     ti/k3-j721e-fpdlink-sk-fusion.dtbo     ti/k3-j721e-sk-csi2-ov5640.dtbo     ti/k3-j721e-sk-rpi-cam-imx219.dtbo      ti/k3-j721e-vision-apps.dtbo     ti/k3-j721e-edgeai-apps.dtbo     ti/k3-j721e-sk-rpi-hdr-ehrpwm.dtbo  ti/k3-j721e-quad-port-eth-exp.dtbo
+
+MAKE_ALL_TARGETS?= arm-benchmarks am-sysinfo oprofile-example cryptodev ti-img-rogue-driver u-boot sysfw-image linux linux-dtbs
