b5e2d99fb4dc3c815f4a06dd230f9ba8341d4c74
Sai Sree Kartheek Adivi <s-adivi@ti.com>
2023-06-15 02:53:05 +0530

    sitara-platforms: add support for AM62x and AM64x
    Signed-off-by: Sai Sree Kartheek Adivi <s-adivi@ti.com>

---
+++ configs/platforms/am62xx-evm.mk
@@ -0,0 +1,23 @@
+#platform
+SOC=am62
+SOC_TYPE?=hs-fs
+
+#defconfig
+DEFCONFIG=tisdk_am62xx-evm_defconfig
+
+ARCH=aarch64
+
+#u-boot machine
+UBOOT_MACHINE=am62x_evm_a53_defconfig
+
+# Add CROSS_COMPILE and UBOOT_MACHINE for the R5
+UBOOT_MACHINE_R5=am62x_evm_r5_defconfig
+
+KERNEL_DEVICETREE?= k3-am625-beagleplay.dtb k3-am625-sk-csi2-ov5640.dtbo k3-am625-sk-csi2-tevi-ov5640.dtbo k3-am625-sk.dtb k3-am625-sk-hdmi-audio.dtbo k3-am625-sk-lpmdemo.dtb k3-am625-sk-mcan.dtbo k3-am625-sk-microtips-mf101hie-panel.dtbo
+
+# Add configs for ti-img-rogue-driver
+PVR_BUILD_DIR=am62_linux
+RGX_BVNC="22.104.208.318"
+WINDOW_SYSTEM=wayland
+
+MAKE_ALL_TARGETS?= arm-benchmarks am-sysinfo oprofile-example cryptodev u-boot linux linux-dtbs ti-img-rogue-driver
---
+++ configs/platforms/am64xx-evm.mk
@@ -0,0 +1,18 @@
+#platform
+SOC=am64
+SOC_TYPE?=hs-fs
+
+#defconfig
+DEFCONFIG=tisdk_am64xx-evm_defconfig
+
+ARCH=aarch64
+
+#u-boot machine
+UBOOT_MACHINE=am64x_evm_a53_defconfig
+
+# Add CROSS_COMPILE and UBOOT_MACHINE for the R5
+UBOOT_MACHINE_R5=am64x_evm_r5_defconfig
+
+KERNEL_DEVICETREE?= k3-am625-beagleplay.dtb k3-am625-sk-csi2-ov5640.dtbo k3-am625-sk-csi2-tevi-ov5640.dtbo k3-am625-sk.dtb k3-am625-sk-hdmi-audio.dtbo k3-am625-sk-lpmdemo.dtb k3-am625-sk-mcan.dtbo k3-am625-sk-microtips-mf101hie-panel.dtbo
+
+MAKE_ALL_TARGETS?= arm-benchmarks am-sysinfo oprofile-example cryptodev u-boot linux linux-dtbs
