0f87789b768bbad4770e5be86eb0c922bacd842a
Chirag Shilwant <c-shilwant@ti.com>
2023-09-11 23:36:26 +0530

    configs: platforms: Add config for am62pxx-evm
    Signed-off-by: Chirag Shilwant <c-shilwant@ti.com>

---
+++ configs/platforms/am62pxx-evm.mk
@@ -0,0 +1,28 @@
+#platform
+SOC=am62p
+
+#add platform for scripts
+PLATFORM?=am62pxx-evm
+
+#defconfig
+DEFCONFIG=tisdk_am62pxx-evm_defconfig
+
+ARCH=aarch64
+
+# u-boot machine configs for A53 and R5
+UBOOT_MACHINE=am62px_evm_a53_defconfig
+UBOOT_MACHINE_R5=am62px_evm_r5_defconfig
+MKIMAGE_DTB_FILE=a53/arch/arm/dts/k3-am62p5-sk.dtb
+
+KERNEL_DEVICETREE_PREFIX=ti/k3-am62p5
+
+TI_LINUX_FIRMWARE=$(TI_SDK_PATH)/board-support/prebuilt-images/$(PLATFORM)
+UBOOT_ATF=$(TI_SDK_PATH)/board-support/prebuilt-images/$(PLATFORM)/bl31.bin
+UBOOT_TEE=$(TI_SDK_PATH)/board-support/prebuilt-images/$(PLATFORM)/bl32.bin
+
+# Add configs for ti-img-rogue-driver
+PVR_BUILD_DIR=am62p_linux
+RGX_BVNC="33.15.11.3"
+WINDOW_SYSTEM=wayland
+
+MAKE_ALL_TARGETS?= arm-benchmarks oprofile-example pru-icss matrix-gui cryptodev u-boot linux linux-dtbs ti-img-rogue-driver
