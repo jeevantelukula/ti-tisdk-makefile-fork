55f018d2f7e0d6ae2444bfc83d5a9709b7b1cadb
Paresh Bhagat <p-bhagat@ti.com>
2023-09-27 11:46:46 +0530

    Add CROSS-COMPILE for individual machines
    Add CROSS-COMPILE for individual machines instead of specifying the same
    in tisdk-installer.mk file which becomes common for all platforms.
    
    This will enable to change ARM toolchain based on whether the platform is
    32-bit or 64-bit.
    
    Signed-off-by: Paresh Bhagat <p-bhagat@ti.com>

--- configs/setup/tisdk-installer.mk
+++ configs/setup/tisdk-installer.mk
@@ -4,13 +4,6 @@ export TI_SDK_PATH?=__SDK__INSTALL_DIR__
 export LINUX_DEVKIT_PATH=$(TI_SDK_PATH)/linux-devkit
 SDK_PATH_TARGET=$(LINUX_DEVKIT_PATH)/sysroots/aarch64-oe-linux
 SDK_PATH_HOST=$(LINUX_DEVKIT_PATH)/sysroots/x86_64-arago-linux
-export CROSS_COMPILE=$(TI_SDK_PATH)/external-toolchain-dir/arm-gnu-toolchain-11.3.rel1-x86_64-aarch64-none-linux-gnu/bin/aarch64-none-linux-gnu-
-export CROSS_COMPILE_ARMV7=$(TI_SDK_PATH)/external-toolchain-dir/arm-gnu-toolchain-11.3.rel1-x86_64-arm-none-linux-gnueabihf/bin/arm-none-linux-gnueabihf-
-
-#Default CC value to be used when cross compiling.  This is so that the
-#GNU Make default of "cc" is not used to point to the host compiler
-export CC=$(CROSS_COMPILE)gcc --sysroot=$(SDK_PATH_TARGET)
-
 
 # The source directories for each component
 ARM_BENCHMARKS_SRC_DIR=$(TI_SDK_PATH)/example-applications/*arm-benchmarks*
