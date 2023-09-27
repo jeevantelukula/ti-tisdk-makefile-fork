9ce49bef3ee283293b2e0bc8e790287f049704b0
Paresh Bhagat <p-bhagat@ti.com>
2023-09-27 15:29:56 +0530

    Add support for top level makefile for am62sip
    Add top level makefile support for rt and non rt linux
    for am62sip
    
    Signed-off-by: Paresh Bhagat <p-bhagat@ti.com>

---
+++ configs/platforms/am62xxsip-evm.mk
@@ -0,0 +1,35 @@
+#platform
+SOC=am62sip
+
+#add platform for scripts
+PLATFORM?=am62xxsip-evm
+
+#Architecture
+ARCH=arm64
+
+#ARM Toolchains
+export CROSS_COMPILE=$(TI_SDK_PATH)/external-toolchain-dir/arm-gnu-toolchain-11.3.rel1-x86_64-aarch64-none-linux-gnu/bin/aarch64-none-linux-gnu-
+export CROSS_COMPILE_ARMV7=$(TI_SDK_PATH)/external-toolchain-dir/arm-gnu-toolchain-11.3.rel1-x86_64-arm-none-linux-gnueabihf/bin/arm-none-linux-gnueabihf-
+
+#Default CC value to be used when cross compiling.  This is so that the
+#GNU Make default of "cc" is not used to point to the host compiler
+export CC=$(CROSS_COMPILE)gcc --sysroot=$(SDK_PATH_TARGET)
+
+# u-boot machine configs for A53 and R5
+UBOOT_MACHINE=am62x_evm_a53_defconfig
+UBOOT_MACHINE_R5=am62x_evm_r5_defconfig
+UBOOT_MACHINE_R5_FRAGMENT=am62xsip_sk_r5.config
+MKIMAGE_DTB_FILE=a53/arch/arm/dts/k3-am625-sk.dtb
+
+KERNEL_DEVICETREE_PREFIX=ti/k3-am625|ti/k3-am62-|ti/k3-am62x|ti/k3-am62.dtsi
+
+TI_LINUX_FIRMWARE=$(TI_SDK_PATH)/board-support/prebuilt-images/$(PLATFORM)
+UBOOT_ATF=$(TI_SDK_PATH)/board-support/prebuilt-images/$(PLATFORM)/bl31.bin
+UBOOT_TEE=$(TI_SDK_PATH)/board-support/prebuilt-images/$(PLATFORM)/bl32.bin
+
+# Add configs for ti-img-rogue-driver
+PVR_BUILD_DIR=am62sip_linux
+RGX_BVNC="33.15.11.3"
+WINDOW_SYSTEM=wayland
+
+MAKE_ALL_TARGETS?= arm-benchmarks oprofile-example pru-icss matrix-gui cryptodev u-boot linux linux-dtbs ti-img-rogue-driver
