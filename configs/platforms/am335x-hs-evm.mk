34026397e60b278bfee94909f1915dac7038cdd9
Paresh Bhagat <p-bhagat@ti.com>
2023-11-10 16:04:14 +0530

    Add makefile support for am335 hs evm
    Add makefile config for am33 hs evm. Also add new makefile
    target secdev which is required for secure boot for am33 and
    am43.
    
    Signed-off-by: Paresh Bhagat <p-bhagat@ti.com>

---
+++ configs/platforms/am335x-hs-evm.mk
@@ -0,0 +1,25 @@
+#platform
+SOC=am335
+TARGET_PRODUCT=ti335x
+
+#add platform for scripts
+PLATFORM?=am335x-hs-evm
+
+ARCH=arm
+
+# u-boot machine config
+UBOOT_MACHINE=am335x_hs_evm_defconfig
+
+# Override value of cross compiler
+export CROSS_COMPILE=$(TI_SDK_PATH)/external-toolchain-dir/arm-gnu-toolchain-11.3.rel1-x86_64-arm-none-linux-gnueabihf/bin/arm-none-linux-gnueabihf-
+export CC=$(CROSS_COMPILE)gcc --sysroot=$(SDK_PATH_TARGET)
+
+# secdev package in the SDK for U-Boot build
+export TI_SECURE_DEV_PKG=$(TI_SDK_PATH)/board-support/proc-sdk-secdev*
+ 
+# Toolchains required by the vairious SECDEV packages
+export GCC_ARM_NONE_TOOLCHAIN=$(LINUX_DEVKIT_PATH)/sysroots/x86_64-arago-linux/usr/share/ti/gcc-arm-none-eabi
+
+KERNEL_DEVICETREE_PREFIX=am335x-
+
+MAKE_ALL_TARGETS?= pru-icss matrix-gui cryptodev secdev u-boot-legacy linux-legacy linux-legacy-dtbs refresh-screen mmwavegesture-hmi evse-hmi protection-relays-hmi ti-sgx-ddk-km
