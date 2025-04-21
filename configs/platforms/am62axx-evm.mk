7059e9b181effab9dda42724fd30e17f4fe7ec82
Telukula Jeevan Kumar Sahu <j-sahu@ti.com>
2025-04-21 15:09:50 +0530

    update am62a makefile configs
    Signed-off-by: Telukula Jeevan Kumar Sahu <j-sahu@ti.com>

--- configs/platforms/am62axx-evm.mk
+++ configs/platforms/am62axx-evm.mk
@@ -1,8 +1,9 @@
 #platform
 SOC=am62a
+SYSFW_SOC=am62ax
 
 #add platform for scripts
-PLATFORM=am62axx-evm
+PLATFORM?=am62axx-evm
 
 ARCH=aarch64
 
@@ -14,4 +15,4 @@ UBOOT_MACHINE_R5=am62ax_evm_r5_defconfig
 
 KERNEL_DEVICETREE?= ti/k3-am62a7-sk.dtb    ti/k3-am62a7-sk-csi2-imx219.dtbo    ti/k3-am62a7-sk-fusion.dtbo    ti/k3-am62a7-sk-ethernet-dc01.dtbo
 
-MAKE_ALL_TARGETS?= arm-benchmarks am-sysinfo oprofile-example cryptodev u-boot linux linux-dtbs
+MAKE_ALL_TARGETS?= cryptodev u-boot linux linux-dtbs
