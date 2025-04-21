40dcffd725b51339e95f4be828d283be5e370097
Telukula Jeevan Kumar Sahu <j-sahu@ti.com>
2025-04-21 15:50:37 +0530

    update configs am62a
    Signed-off-by: Telukula Jeevan Kumar Sahu <j-sahu@ti.com>

--- configs/platforms/am62axx-evm.mk
+++ configs/platforms/am62axx-evm.mk
@@ -7,12 +7,24 @@ PLATFORM?=am62axx-evm
 
 ARCH=aarch64
 
-#u-boot machine
-UBOOT_MACHINE=am62ax_evm_a53_defconfig
+#Set cross compilers
+export CROSS_COMPILE=$(TI_SDK_PATH)/linux-devkit/sysroots/x86_64-arago-linux/usr/bin/aarch64-oe-linux/aarch64-oe-linux-
+export CROSS_COMPILE_ARMV7=$(TI_SDK_PATH)/k3r5-devkit/sysroots/x86_64-arago-linux/usr/bin/arm-oe-eabi/arm-oe-eabi-
 
-# Add CROSS_COMPILE and UBOOT_MACHINE for the R5
+#Default CC value to be used when cross compiling.  This is so that the
+#GNU Make default of "cc" is not used to point to the host compiler
+export CC_64=$(CROSS_COMPILE)gcc --sysroot=$(SDK_PATH_TARGET)
+
+# u-boot machine configs for A53 and R5
+UBOOT_MACHINE=am62ax_evm_a53_defconfig
 UBOOT_MACHINE_R5=am62ax_evm_r5_defconfig
+MKIMAGE_DTB_FILE=a53/dts/upstream/src/arm64/ti/k3-am62a7-sk.dtb
 
 KERNEL_DEVICETREE?= ti/k3-am62a7-sk.dtb    ti/k3-am62a7-sk-csi2-imx219.dtbo    ti/k3-am62a7-sk-fusion.dtbo    ti/k3-am62a7-sk-ethernet-dc01.dtbo
+
+
+TI_LINUX_FIRMWARE=$(TI_SDK_PATH)/board-support/prebuilt-images/$(PLATFORM)
+UBOOT_ATF=$(TI_SDK_PATH)/board-support/prebuilt-images/$(PLATFORM)/bl31.bin
+UBOOT_TEE=$(TI_SDK_PATH)/board-support/prebuilt-images/$(PLATFORM)/bl32.bin
 
 MAKE_ALL_TARGETS?= cryptodev u-boot linux linux-dtbs
